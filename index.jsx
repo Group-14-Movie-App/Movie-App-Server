8105fb2c9ae3ddef89106509f20f1bbe75959a4f
XuanzhaoS <t3zhxu00@students.oamk.fi>
2024-12-10 10:11:07 +0200

    Backend Deployment Test

--- index.jsx
+++ index.jsx
@@ -1,73 +1,74 @@
-const express = require('express');
+// For deployment test
+require("dotenv").config();
+
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
+// For deployment test
+const dbHost = process.env.DB_HOST || "no-database";
+if (dbHost === "no-database") {
+  console.log("Warning: No database connected, using mock data.");
+}
+
+const cors = require("cors");
 app.use(cors());
 
 app.use(express.json());
 
-const bodyParser = require('body-parser'); // Parse incoming requests
+const bodyParser = require("body-parser"); // Parse incoming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Routes Definition do here
-const registerRouter = require('./routes/registerRoute.js');
-app.use('/register', registerRouter);
+const registerRouter = require("./routes/registerRoute.js");
+app.use("/register", registerRouter);
 
+const reviewRouter = require("./routes/reviewRoute.js");
+app.use("/reviews", reviewRouter);
 
+const signinRouter = require("./routes/signinRoute.js");
+app.use("/signin", signinRouter);
 
-const reviewRouter = require('./routes/reviewRoute.js')
-app.use('/reviews', reviewRouter)
+const profileRouter = require("./routes/profileRoute.js");
+app.use("/profile", profileRouter);
 
-const signinRouter = require('./routes/signinRoute.js');
-app.use('/signin', signinRouter);
+const favoritesRouter = require("./routes/favoritesRoute.js");
+app.use("/favorites", favoritesRouter);
 
-const profileRouter = require('./routes/profileRoute.js')
-app.use('/profile', profileRouter)
+const favoriteMoviesRouter = require("./routes/favoriteMoviesRoute.js");
+app.use("/favorite-movies", favoriteMoviesRouter);
 
-const favoritesRouter = require('./routes/favoritesRoute.js');
-app.use('/favorites', favoritesRouter);
+const CreateGroupRouter = require("./routes/CreateGroupRoute.js");
+app.use("/groups", CreateGroupRouter);
 
-const favoriteMoviesRouter = require('./routes/favoriteMoviesRoute.js');
-app.use('/favorite-movies', favoriteMoviesRouter);
+const myGroupsRouter = require("./routes/myGroupsRoute.js");
+app.use("/my-groups", myGroupsRouter);
 
+const mygroupDetailsRouter = require("./routes/mygroupDetailsRoute.js");
+app.use("/groups", mygroupDetailsRouter);
 
+const allGroupsRouter = require("./routes/allGroupsRoute.js");
+app.use("/all-groups", allGroupsRouter);
 
-
-
-const CreateGroupRouter = require('./routes/CreateGroupRoute.js');
-app.use('/groups', CreateGroupRouter);
-
-const myGroupsRouter = require('./routes/myGroupsRoute.js');
-app.use('/my-groups', myGroupsRouter);
-
-const mygroupDetailsRouter = require('./routes/mygroupDetailsRoute.js');
-app.use('/groups', mygroupDetailsRouter);
-
-const allGroupsRouter = require('./routes/allGroupsRoute.js');
-app.use('/all-groups', allGroupsRouter);
-
-const groupJoinRequestsRouter = require('./routes/groupJoinRequestsRoute.js');
-app.use('/group-join-requests', groupJoinRequestsRouter);
+const groupJoinRequestsRouter = require("./routes/groupJoinRequestsRoute.js");
+app.use("/group-join-requests", groupJoinRequestsRouter);
 
 const otherGroupDetailsRouter = require("./routes/otherGroupDetailsRoute.js");
 app.use("/other-groups", otherGroupDetailsRouter);
 
-const otherGroupPostsRouter = require("./routes/otherGroupPostsRoute.js")
+const otherGroupPostsRouter = require("./routes/otherGroupPostsRoute.js");
 app.use("/group-posts", otherGroupPostsRouter);
 
 const myGroupPostsRouter = require("./routes/myGroupPostsRoute.js");
 app.use("/my-group-posts", myGroupPostsRouter);
 
-
-const chatbotRouter = require('./routes/chatbot.js')
-app.use('/chatbot', chatbotRouter);
+const chatbotRouter = require("./routes/chatbot.js");
+app.use("/chatbot", chatbotRouter);
 
 //export app for testing
 module.exports = app;
 
-const port = 5000
-app.listen(port, ()=>
-{
-    console.log(`Server Listining to "http://localhost:${port}"`);
-})
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server Listining to "http://localhost:${port}"`);
+});
