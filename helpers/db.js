3a16e5f1de256da23b5d71e1e5857f960e7e7954
Ryan_DIN23SP <ryandilthusha@gmail.com>
2024-12-11 14:45:40 +0200

    .env files deployed

--- helpers/db.js
+++ helpers/db.js
@@ -11,7 +11,8 @@ const pool = new Pool(
         port:process.env.DB_PORT,
 
         user:process.env.DB_USER,
-        password:process.env.DB_PASSWORD
+        password:process.env.DB_PASSWORD,
+        ssl:process.env.SSL
     }
 )
 
