e3ee3501eebe9f3f6c3043de3a0a957a9f1172db
XuanzhaoS <t3zhxu00@students.oamk.fi>
2024-12-11 11:37:15 +0200

    Add ssl for backend deployment

--- helpers/db.js
+++ helpers/db.js
@@ -11,7 +11,11 @@ const pool = new Pool(
         port:process.env.DB_PORT,
 
         user:process.env.DB_USER,
-        password:process.env.DB_PASSWORD
+        password:process.env.DB_PASSWORD,
+        // For Backend deployment
+        ssl:{
+            rejectUnauthorized: false
+        }
     }
 )
 
