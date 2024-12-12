071fcf3efc426f6b7d1dc1362cfd7bc932843d0f
Ryan_DIN23SP <ryandilthusha@gmail.com>
2024-12-12 06:04:15 +0200

    Review part issue fix

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -251,6 +251,7 @@ reviewRouter.get('/public', async (req, res) => {
         u.lastName 
       FROM Reviews r
       JOIN Users u ON r.userID = u.userID
+      ORDER BY r.timestamp DESC
     `;
     const result = await pool.query(query);
 
